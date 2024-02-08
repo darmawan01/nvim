7602c9a94d7d7a540319b341f60fafc5d4924b1b
Dz <darmawan.zulkifli01@gmail.com>
2024-02-08 08:13:28 +0800

    update plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,10 +4,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "gruvbox",
-      -- colorscheme = "nord",
       colorscheme = "tokyonight",
-      -- colorscheme = "catppuccin-macchiato", -- catppuccin-latte, catppuccin-faappe, catppuccin-macchiato, catppuccin-mocha
     },
   },
 }
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -8,4 +8,23 @@ return {
       end,
     },
   },
+
+  {
+    "Exafunction/codeium.vim",
+    lazy = false,
+    config = function()
+      vim.keymap.set("i", "<tab>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
 }
