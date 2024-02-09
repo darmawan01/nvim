be0b0c2e2b729f7cff23dcbd07ea36d764063980
Darmawan Zulkifli <23370707+darmawan01@users.noreply.github.com>
2024-02-09 18:07:57 +0800

    setup kickstart lsp config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,5 +7,24 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".DS_Store",
+          },
+          never_show = {
+            ".git",
+            "node_modules",
+            ".DS_Store",
+            "vscode",
+          },
+        },
+      },
+    },
   },
 }
