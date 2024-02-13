005052bbf9e537168264e5accc858903672c73d1
Dz <darmawan.zulkifli01@gmail.com>
2024-02-13 13:26:36 +0800

    reset lazyvim setup

---
+++ lua/plugins/tree.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".DS_Store",
+          },
+          never_show = {
+            ".git",
+            "node_modules",
+            ".DS_Store",
+            "vscode",
+          },
+        },
+      },
+    },
+  },
+}
